13e4a272cd29a7591fcc54243f2d9d30b4b997a4
NaulaN <nonochrzaszcz@gmail.com>
2023-04-02 12:47:01 +0200

    add onStart

--- app/src/main/java/fr/naulantiago/saeandroid/MainActivity.java
+++ app/src/main/java/fr/naulantiago/saeandroid/MainActivity.java
@@ -5,17 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TableLayout;
 
+import java.util.List;
+
 import fr.naulantiago.saeandroid.model.FetchPokemons;
+import fr.naulantiago.saeandroid.model.PokemonData;
 
 public class MainActivity extends AppCompatActivity {
     private TableLayout mTableLayout;
+    private List<PokemonData> pokemonData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        mTableLayout = findViewById(R.id.tablePokedex);
+        this.mTableLayout = findViewById(R.id.tablePokedex);
+
         FetchPokemons toast = new FetchPokemons();
-        System.out.println(toast.getPokemonDatas());
+        this.pokemonData = toast.getPokemonDatas();
+        System.out.println(this.pokemonData);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        this.addPokemonsToTable();
     }
 
     private void addPokemonsToTable() {
