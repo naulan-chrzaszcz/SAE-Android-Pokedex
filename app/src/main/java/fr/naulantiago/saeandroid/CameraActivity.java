654fc8333784588ca3902f3c907ef5721781f861
almerion <tig2468.a@gmail.com>
2023-04-17 18:36:51 +0200

    fix truc

--- app/src/main/java/fr/naulantiago/saeandroid/CameraActivity.java
+++ app/src/main/java/fr/naulantiago/saeandroid/CameraActivity.java
@@ -89,9 +89,9 @@ public class CameraActivity extends AppCompatActivity {
             return;
         }
         ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE, "My Image");
+        values.put(MediaStore.Images.Media.TITLE, "Evoli camera");
         values.put(MediaStore.Images.Media.DISPLAY_NAME, fileName);
-        values.put(MediaStore.Images.Media.DESCRIPTION, "Image captured with CameraView");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Pokemon camera");
         values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
         values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
         values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
